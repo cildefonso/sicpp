18534b3795971a9a3d89f00717e494e53bffb380
Carlos Ildefonso <cildefonso@gmail.com>
2021-08-18 19:24:11 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/api/sicpp/SicppApplication.java
@@ -0,0 +1,13 @@
+package com.api.sicpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/sicpp/SicppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sicpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
